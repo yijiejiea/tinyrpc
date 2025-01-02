e54afc4138644d52aa8abcbe2635eedc96dc4d32
宇宙无敌炫酷大超人一杰 <61676061@qq.com>
2025-01-03 01:10:22 +0800

    style

--- tinyrpc/comm/log.cc
+++ tinyrpc/comm/log.cc
@@ -53,23 +53,24 @@ namespace tinyrpc
     return LogLevel::DEBUG;
   }
 
-  void initLogger(const std::string& log_file_path, spdlog::level::level_enum log_level) {
+  void initLogger(const std::string &log_file_path, spdlog::level::level_enum log_level)
+  {
     // 创建控制台输出（有颜色）
     auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-    console_sink->set_level(log_level);  // 控制台日志级别
+    console_sink->set_level(log_level); // 控制台日志级别
 
     // 创建文件输出（日志保存到文件）
     auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(log_file_path, true);
-    file_sink->set_level(log_level);  // 文件日志级别
+    file_sink->set_level(log_level); // 文件日志级别
 
     // 创建 gRpcLogger
     gRpcLogger = std::make_shared<spdlog::logger>("rpc_logger", spdlog::sinks_init_list{console_sink, file_sink});
-    gRpcLogger->set_level(log_level);  // 全局日志级别
+    gRpcLogger->set_level(log_level); // 全局日志级别
 
     // 创建 gAppLogger，使用不同的文件或输出
     auto app_file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("app_" + log_file_path, true);
     gAppLogger = std::make_shared<spdlog::logger>("app_logger", app_file_sink);
-    gAppLogger->set_level(log_level);  // 可以设置不同的日志级别
-}
+    gAppLogger->set_level(log_level); // 可以设置不同的日志级别
+  }
 
 }
--- tinyrpc/comm/log.h
+++ tinyrpc/comm/log.h
@@ -7,78 +7,83 @@
 #include <string>
 #include <filesystem>
 #include <sstream>
-#include <fmt/core.h>  // fmt 库用于格式化支持
+#include <fmt/core.h> // fmt 库用于格式化支持
 
 #include "tinyrpc/comm/config.h"
 
-namespace tinyrpc {
+namespace tinyrpc
+{
 
-// 外部日志对象声明
-extern std::shared_ptr<spdlog::logger> gRpcLogger;
-extern std::shared_ptr<spdlog::logger> gAppLogger;
+  // 外部日志对象声明
+  extern std::shared_ptr<spdlog::logger> gRpcLogger;
+  extern std::shared_ptr<spdlog::logger> gAppLogger;
 
-LogLevel stringToLevel(const std::string& str);
-std::string levelToString(LogLevel level);
-void initLogger(const std::string& log_file_path = "tinyrpc.log", spdlog::level::level_enum log_level = spdlog::level::debug);
+  LogLevel stringToLevel(const std::string &str);
+  std::string levelToString(LogLevel level);
+  void initLogger(const std::string &log_file_path = "tinyrpc.log", spdlog::level::level_enum log_level = spdlog::level::debug);
 
-// 日志流类
-class LoggerStream {
-public:
+  // 日志流类
+  class LoggerStream
+  {
+  public:
     // 接受一个日志级别来构造
     LoggerStream(spdlog::level::level_enum level) : oss(), level(level) {}
 
     // 析构时输出日志
-    ~LoggerStream() {
-        if (gRpcLogger) {
-            gRpcLogger->log(level, oss.str());
-        }
+    ~LoggerStream()
+    {
+      if (gRpcLogger)
+      {
+        gRpcLogger->log(level, oss.str());
+      }
     }
 
     // 支持格式化输出
     template <typename... Args>
-    LoggerStream& operator()(const char* format, Args&&... args) {
-        oss << fmt::format(format, std::forward<Args>(args)...);
-        return *this;
+    LoggerStream &operator()(const char *format, Args &&...args)
+    {
+      oss << fmt::format(format, std::forward<Args>(args)...);
+      return *this;
     }
 
     // 支持流式输出
     template <typename T>
-    LoggerStream& operator<<(const T& value) {
-        oss << value;  // 构建日志信息
-        return *this;
+    LoggerStream &operator<<(const T &value)
+    {
+      oss << value; // 构建日志信息
+      return *this;
     }
 
-private:
-    std::ostringstream oss;  // 用于拼接日志信息的流
-    spdlog::level::level_enum level;  // 日志级别
-};
-
+  private:
+    std::ostringstream oss;          // 用于拼接日志信息的流
+    spdlog::level::level_enum level; // 日志级别
+  };
 
 // 日志宏定义
 // RPC 日志宏定义
 #define DebugLog tinyrpc::gRpcLogger->debug("[{}:{}] [debug] ", __FILE__, __LINE__), \
-  tinyrpc::LoggerStream(spdlog::level::debug)
+                 tinyrpc::LoggerStream(spdlog::level::debug)
 
 #define InfoLog tinyrpc::gRpcLogger->info("[{}:{}] [info] ", __FILE__, __LINE__), \
-  tinyrpc::LoggerStream(spdlog::level::info)
+                tinyrpc::LoggerStream(spdlog::level::info)
 
 #define WarnLog tinyrpc::gRpcLogger->warn("[{}:{}] [warning] ", __FILE__, __LINE__), \
-  tinyrpc::LoggerStream(spdlog::level::warn)
+                tinyrpc::LoggerStream(spdlog::level::warn)
 
 #define ErrorLog tinyrpc::gRpcLogger->error("[{}:{}] [error] ", __FILE__, __LINE__), \
-  tinyrpc::LoggerStream(spdlog::level::err)
+                 tinyrpc::LoggerStream(spdlog::level::err)
 
 // 应用程序日志宏定义
 #define AppDebugLog tinyrpc::gAppLogger->debug("[{}:{}] [debug] ", __FILE__, __LINE__), \
-  tinyrpc::LoggerStream(spdlog::level::debug)
+                    tinyrpc::LoggerStream(spdlog::level::debug)
 
 #define AppInfoLog tinyrpc::gAppLogger->info("[{}:{}] [info] ", __FILE__, __LINE__), \
-  tinyrpc::LoggerStream(spdlog::level::info)
+                   tinyrpc::LoggerStream(spdlog::level::info)
 
 #define AppWarnLog tinyrpc::gAppLogger->warn("[{}:{}] [warning] ", __FILE__, __LINE__), \
-  tinyrpc::LoggerStream(spdlog::level::warn)
+                   tinyrpc::LoggerStream(spdlog::level::warn)
 
 #define AppErrorLog tinyrpc::gAppLogger->error("[{}:{}] [error] ", __FILE__, __LINE__), \
-  tinyrpc::LoggerStream(spdlog::level::err)
+                    tinyrpc::LoggerStream(spdlog::level::err)
 
-}  // namespace tinyrpc
+} // namespace tinyrpc
